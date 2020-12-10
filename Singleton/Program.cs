b5b145a86b03c3a3ddfed716b8062d0dfa06c67e
Hayk <hayk.hayr3@gmail.com>
2020-12-10 23:21:30 +0400

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance();
+            Console.WriteLine(Singleton.Instance());
+
+        }
+    }
+    class Singleton
+    {
+        static Singleton instance = null;
+        protected Singleton() { }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+
+}
